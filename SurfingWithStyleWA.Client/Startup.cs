a06d521a084a8c914a1989ec28ad48f9bad2388c
Paul Williams <paulwilliams_redmond@hotmail.com>
2018-09-18 12:26:40 -0700

    fix: page names

--- SurfingWithStyleWA.Client/Startup.cs
+++ SurfingWithStyleWA.Client/Startup.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Blazor.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace SurfingWithStyleWA.Client
@@ -7,6 +8,7 @@ namespace SurfingWithStyleWA.Client
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         }
 
         public void Configure(IBlazorApplicationBuilder app)
