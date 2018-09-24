ef64bfbc2838b375fb638aaf9d6afdf4219933cd
Paul Williams <paulwilliams_redmond@hotmail.com>
2018-09-23 19:36:09 -0700

    fix: Metronome pause

---
+++ SurfingWithStyleWA.Client/Pages/Practice/AnimationHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.JSInterop;
+using System;
+
+namespace SurfingWithStyleWA.Client.Pages.Practice
+{
+    public class AnimationHelper
+    {
+        Action<string> SetAnimationName;
+
+        public AnimationHelper(Action<string> setAnimationName)
+        {
+            this.SetAnimationName = setAnimationName;
+        }
+
+        [JSInvokable]
+        public void SetAnimationToSwing()
+        {
+            SetAnimationName("swing");
+        }
+    }
+}
