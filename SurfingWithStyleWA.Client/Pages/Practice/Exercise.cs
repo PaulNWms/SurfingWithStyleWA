fdca7cf8900b11dd73218e371c771e3531e64dbb
Paul Williams <paulwilliams_redmond@hotmail.com>
2018-09-26 11:20:17 -0700

    refactor: ScheduledMetronome: move Exercise to .cs file

---
+++ SurfingWithStyleWA.Client/Pages/Practice/Exercise.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SurfingWithStyleWA.Client.Pages.Practice
+{
+    struct Exercise
+    {
+        public int Tempo;
+        public TimeSpan Duration;
+        public string Description;
+    }
+}
